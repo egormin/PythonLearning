af54923d79d0873e5066953fba98bb3c0f4aff8b
egormin <neoman2014@gmail.com>
2016-12-18 22:29:12 +0300

    Commit 18.12.16 22:29

--- Variables.py
+++ Variables.py
@@ -1,1 +1,36 @@
-a = "Hello"
+# 1)How to define variable type
+a = 55
+type(a)
+type("efsf")
+#<class 'str'>, <class 'int'>
+
+# 2)Convert binary to decimal
+0b11111111
+
+# 3)Convert oct to decimal
+0o45466
+
+# 4)Convert oct to decimal
+print(0x45466FA)
+
+# 5)Output variable
+number=546346
+print(number)
+
+# 6)Result of previous operation (available only in console)
+#type(_)
+
+# 7)Convert string to int
+string_value="15"
+result=int(string_value)
+print(result)
+
+# 8)Convert string to int and add 5
+string_value="15"
+result=int(string_value)
+print(result + 5)
+
+
+
+
+
