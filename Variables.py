6ef3610c009653af15ebca95d0408377387c10dd
egormin <neoman2014@gmail.com>
2016-12-19 00:02:21 +0300

    Commit 19.12.16 00:02

--- Variables.py
+++ Variables.py
@@ -30,6 +30,98 @@ string_value="15"
 result=int(string_value)
 print(result + 5)
 
+# 9)True is 1, False is 0
+bool1=True
+bool2=False
+print(bool1,bool2)
+print(bool1+bool2)
+print(bool1+7)
+
+# 10)Float values
+a=0.5
+print(type(a)) #<class 'float'>
+
+# 10)Also float values
+a=1.0
+b=4.
+print(type(a),type(b)) #<class 'float'><class 'float'>
+
+# 11)Exponential (3.2 - mantissa, e5 - exponenta
+a=3.2e5 #this equal 320000.0
+print(a)
+b=3.7e-3 #this equal 0.0037
+print(b)
+
+# 12)Convert int to float, float to int
+a=10; print(type(a)) #int
+b=float(a);print(type(b)) #float
+c=int(b);print(type(c)) #and again int
+d=int(5.4); print(d) #will be 5 (rounded)
+
+# 13)Dynamic typing
+a="String value"
+print(type(a))  #<class 'str'>
+a=10
+print(type(a)) #<class 'int'>
+
+# 13)Strong typing
+a='5' #string
+b=10
+#print(a+b) #error
+
+# 13)Operations
+a=2+3*5-1
+print(a)
+a=6//4 #celochislennoe delenie
+print(a) #1
+a=6%4 #ostatok ot deleniya
+print(a) #2
+
+# 14)Another operations
+a=-6
+print(abs(a)) #po moduliu (menyaet - na +
+a=2**4 # 2 v stepeni 4
+print(a) #16
+a=pow(2,4)# 2 v stepeni 4
+print(a) #16
+b=3.444
+print(round(b)) #=3, okruglenie
+print(round(b,2)) #=3.44, okruglenie lj 2 znakov posle zapyatoi
+
+# 14)Python2 and Python3 differs
+print(2/8) #in Python2 = 0 (because result is int)
+print(2/8) #in Python3 = 0.25 (because result is float)
+print(float(2)/8) #Wright way for Python2
+
+# 15)Arithmetic operations
+import math
+x = 3.265
+print('x=', x)
+print('trunc x =', math.trunc(x)) #otbrasivaet drobnuyu chast
+print('floor x =', math.floor(x)) #round to lower value
+print('ceil x =', math.ceil(x)) #round to higher value
+print(math.pi) #Pi value
+print(math.sin(math.pi/4)) #Pi value
+print(math.cos(10))
+print(math.sqrt(16))
+
+# 15)Logic Operations
+print(True and False) #false
+print(True or False) #true
+print(not True and(True or False))#false
+print(2>3)#false
+print(5 == 5)#true (Equal)
+print(5 != 5)#false (Not equal)
+print(2 is 2) #True The same place in memory
+print(id(a)) #Show place in memory(for ex. 18668160)
+
+
+
+
+
+
+
+
 
 
 
---
+++ calc.py
@@ -0,0 +1,5 @@
+a=int(input("Input first value: "))
+b=int(input("Input second value: "))
+c=input("Input operation: ")
+result=a + b
+print("a + b = ", result)
