cb55a1186c5960116dd30930a4be03386a3df1ad
egormin <neoman2014@gmail.com>
2018-05-30 10:43:04 +0300

    Update files.py

--- files.py
+++ files.py
@@ -11,3 +11,26 @@ myfile.close()
 with open('file.txt') as myfile:
     for line in myfile:
         print(line)
+        
+#3 sposob       
+for line in open('file.txt'):
+    print(line.strip())
+    
+myfile = open('file.txt') 
+myfile.readline()       # read only one line
+myfile.read()           # read all file
+myfile.readlines()      # read all lines in 
+myfile.seek()           # return to first line
+myfile.close()          # close file
+    
+### Write to file:
+#1 sposob
+myfile = open('fl.txt', 'w')
+myfile.write("first string \n")
+myfile.write('second string')
+print("text", file=myfile)
+myfile.close()
+
+#2 sposob   With context manager
+with open('fl.txt', 'w') as file:
+    file.write('Hello')
